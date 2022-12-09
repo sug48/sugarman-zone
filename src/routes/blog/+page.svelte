d9ff98243694b1d55201ada9385476505400ae4a
sug48 <mpsugarman@gmail.com>
2022-12-08 23:39:21 -0500

    12-8 commit

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,20 @@
+
+<div>
+    <h1>blog post</h1>
+    <p>
+        nkgdsnklsdflkdfslknfsdlknsdfklndfskln
+    </p>
+</div>
+
+<style>
+    div {
+        min-height: 100vh;
+        width: 60vw;
+        background: rgba(255, 255, 255, 0.433);
+        padding: 5vh 5vw;
+    }
+
+    p {
+        color: black;
+    }
+</style>
